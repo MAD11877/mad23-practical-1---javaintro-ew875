2c32b212a88287e914578ea2778a5f13c5fd2fea
ew875 <s10222221@connect.np.edu.sg>
2023-04-25 14:20:49 +0800

    Q1-5

--- Question4.java
+++ Question4.java
@@ -4,20 +4,16 @@ public class Question4
 {
   public static void main(String[] args)
   {
-    /**
-     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
-     *    e.g.
-     *     > 5
-     *     *****
-     *     ****
-     *     ***
-     *     **
-     *     *
-     * 
-     * Hint: 2 loops are required. System.out.print(...) to print on single line.
-     */
-     
     Scanner in = new Scanner(System.in);
-    
+    int base = in.nextInt();
+    int x, y;
+    for(x = base; x >= 1; x--)
+    {
+      for(y = x; y >= 1; y--)
+      {
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
   }
 }
--- Question5.java
+++ Question5.java
@@ -4,29 +4,45 @@ public class Question5
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
     Scanner in = new Scanner(System.in);
-    
+    int n = in.nextInt(), c;
+    int [] a = new int[n];
+    int [] b = new int[n];
+    double sum = 0;
+    for(int i=0;i<n;i++)
+    {
+      a[i] = in.nextInt();
+    }
+    {
+      for(int i=0;i<n;i++)
+      {
+        c=1;
+        if(a[i]==-1)
+          b[i]=0;
+        else
+        {
+          for(int j=i+1;j<n;j++)
+          {
+            if(a[i]==a[j])
+            {
+              c++;
+              a[j]=-1;
+            }
+          }
+          b[i]=c;
+        }
+      }
+      int m=b[0];
+      for(int i=1;i<n;i++)
+      {
+        if(b[i]>=m)
+          m=b[i];
+      }
+      for(int i=0;i<n;i++)
+      {
+        if(b[i]==m)
+          System.out.println(a[i]);
+      }
+    }
   }
 }
